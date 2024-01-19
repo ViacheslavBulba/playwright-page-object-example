5d649f72dc2537f5273282dd1ab6d8859ecbe0a1
Viacheslav Bulba <megadevices@gmail.com>
2024-01-19 14:42:36 -0800

    non page object example for reference

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,8 +2,23 @@ const { test, expect } = require('@playwright/test');
 const { HomePage } = require('../pom/HomePage');
 const { SearchResultsPage } = require('../pom/SearchResultsPage');
 
+// test(`non page object example for reference`, async ({page}) => {
+//   // define test parameters, input values
+//   const productToSearch = 'Apple - AirPods Pro (2nd generation) with MagSafe Case (USB‑C) - White';
+//   const locatorSearchInput = '.search-input';
+//   const locatorProductLinks = '.sku-title a';
+//   // test scenario steps below
+//   await page.goto(`https://bestbuy.com`);
+//   await page.locator(locatorSearchInput).fill(productToSearch);
+//   await page.locator(locatorSearchInput).press('Enter');
+//   await page.waitForTimeout(5000);
+//   expect(await page.locator(locatorProductLinks).first().textContent()).toBe(productToSearch);
+// });
+
 test('best buy search test - search by full exact name - playwright - page object', async ({ page }) => {
+  // define test parameters, input values
   const productToSearch = 'Apple - AirPods Pro (2nd generation) with MagSafe Case (USB‑C) - White';
+  // test scenario steps below
   const homePage = new HomePage(page);
   await homePage.open();
   await homePage.search(productToSearch);
