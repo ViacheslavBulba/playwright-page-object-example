eb975f802025cace9deb6e27b8446389329eb3a9
Viacheslav Bulba <megadevices@gmail.com>
2024-05-28 10:12:42 -0700

    replaced $$ with locator

--- utils/WebElementUtils.js
+++ utils/WebElementUtils.js
@@ -1,5 +1,5 @@
 export async function getTextFromElements(page, locator) {
-  const elements = await page.$$(locator);
+  const elements = await page.locator(locator).all();
   const texts = [];
   for (let el of elements) {
     texts.push((await el.textContent()).trim());
@@ -8,12 +8,11 @@ export async function getTextFromElements(page, locator) {
 }
 
 export async function getNumberOfElements(page, locator) {
-  const result = (await page.$$(locator)).length;
-  return result;
+  return await page.locator(locator).count();
 }
 
 export async function isElementPresent(page, locator) {
-  return (await page.$$(locator)).length > 0
+  return await page.locator(locator).count() > 0;
 }
 
 export async function getTextFromElement(page, locator) {
