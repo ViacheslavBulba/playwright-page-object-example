c5829f541d967cd616ccc1079a258d88ae26737e
Viacheslav Bulba <megadevices@gmail.com>
2024-08-07 23:04:55 -0700

    WIP - api mock

---
+++ test-data/best-buy-pdp-mock.json.data.js
@@ -0,0 +1,75 @@
+export const bestBuyPdpMock =
+{
+  "data": {
+    "skuId": "6447382",
+    "regularPrice": 11.11,
+    "currentPrice": 11.11,
+    "priceEventType": "regular",
+    "totalSavings": 0.00,
+    "totalSavingsPercent": 0,
+    "totalPaidMemberSavings": 0,
+    "totalNonPaidMemberSavings": 0.00,
+    "customerPrice": 11.11,
+    "isMAP": false,
+    "isPriceMatchGuarantee": true,
+    "offerQualifiers":
+      [
+        {
+          "offerId": "634974",
+          "offerName": "Apple - Apple Music 3 Month Trial GWP",
+          "offerVersion": 696224,
+          "offerDiscountType": "Free",
+          "id": 634974002,
+          "comOfferType": "FREEITEM",
+          "comRuleType": "10",
+          "instanceId": 4,
+          "offerRevocableOnReturns": true,
+          "excludeFromBundleBreakage": false
+        },
+        {
+          "offerId": "529900",
+          "offerName": "Apple Arcade  4 month free",
+          "offerVersion": 696222,
+          "offerDiscountType": "Free",
+          "id": 529900002,
+          "comOfferType": "FREEITEM",
+          "comRuleType": "10", "instanceId": 3,
+          "offerRevocableOnReturns": true,
+          "excludeFromBundleBreakage": false
+        },
+        {
+          "offerId": "476799",
+          "offerName": "Apple fitness 3 month acc", "offerVersion": 696217, "offerDiscountType": "Free",
+          "id": 476799002, "comOfferType": "FREEITEM", "comRuleType": "10", "instanceId": 2, "offerRevocableOnReturns": true, "excludeFromBundleBreakage": false
+        },
+        {
+          "offerId": "465099", "offerName": "Apple TV+ 3 months Purchase a select device and get Apple TV+ 3 months free",
+          "offerVersion": 696216,
+          "offerDiscountType": "Free", "id": 465099002,
+          "comOfferType": "FREEITEM", "comRuleType": "10", "instanceId": 1, "offerRevocableOnReturns": true,
+          "excludeFromBundleBreakage": false
+        }
+      ],
+    "giftSkus": [{ "skuId": "6484511", "quantity": 1, "offerId": "465099", "savings": 0.00, "isRequiredWithOffer": false }, { "skuId": "6489577", "quantity": 1, "offerId": "476799", "savings": 0.00, "isRequiredWithOffer": false }, { "skuId": "6515328", "quantity": 1, "offerId": "529900", "savings": 0.00, "isRequiredWithOffer": false },
+    { "skuId": "6562011", "quantity": 1, "offerId": "634974", "savings": 0.00, "isRequiredWithOffer": false }], "totalGiftSavings": 0.00,
+    "gspUnitPrice": 11.11,
+    "connectionType": "3",
+    "paymentOptions": [{
+      "vendor": "zip", "type": "BNPL", "numberOfInstallments": 4, "installmentServiceFee": 1.00, "installmentAmount": 62.50, "totalServiceFee": 4.00,
+      "totalCost": 11.11, "isDisplayable": true
+    }], "dotComDisplayStatus": "active", "whatIfPrice": {
+      "planPaidMember2": {
+        "price": 229.99, "savings": 20.00, "savingsPercent": 8
+      },
+      "planPaidMember3": { "price": 229.99, "savings": 20.00, "savingsPercent": 8 }
+    },
+    "locationId": "960",
+    "currentAsOfDate": "2024-07-13T23:32:15 GMT",
+    "preferredBadging": "PriceMatchGuarantee",
+    "hasTotalTechWarranty": true,
+    "showTotalSavings": false,
+    "skuType": "hardgood",
+    "categoryId": "pcmcat144700050004"
+  }
+}
+
