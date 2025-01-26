cc2e242c959ab08fc431fb0e7c6480a73effe739
Viacheslav Bulba <megadevices@gmail.com>
2025-01-26 12:42:31 -0800

    cleanup

--- utils/StringUtils.js
+++ utils/StringUtils.js
@@ -1,3 +1,11 @@
 export function getDateAndTimeInString() {
   return (new Date()).toLocaleString().replace(/(:|\/)/g, '-').replace(/,/g, '').replace(/\s/g, '_');
 }
+
+export function getTime() {
+  return new Date().toLocaleTimeString();
+}
+
+export function getDate() {
+  return (new Date().toLocaleDateString()).replace(/(\/)/g, '_');
+}
